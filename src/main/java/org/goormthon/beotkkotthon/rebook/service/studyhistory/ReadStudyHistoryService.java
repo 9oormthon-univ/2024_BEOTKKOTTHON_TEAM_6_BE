78b936577600deaedfac2b1b620b36f3144c2c6e
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-03-24 03:15:25 +0900

    fix: API Error

--- src/main/java/org/goormthon/beotkkotthon/rebook/service/studyhistory/ReadStudyHistoryService.java
+++ src/main/java/org/goormthon/beotkkotthon/rebook/service/studyhistory/ReadStudyHistoryService.java
@@ -2,41 +2,29 @@ package org.goormthon.beotkkotthon.rebook.service.studyhistory;
 
 import lombok.RequiredArgsConstructor;
 import org.goormthon.beotkkotthon.rebook.domain.StudyHistory;
-import org.goormthon.beotkkotthon.rebook.dto.request.StudyHistoryRequestDto;
 import org.goormthon.beotkkotthon.rebook.dto.response.studyhistory.StudyHistoryDetailDto;
-import org.goormthon.beotkkotthon.rebook.dto.response.studyhistory.StudyHistoryListDto;
 import org.goormthon.beotkkotthon.rebook.exception.CommonException;
 import org.goormthon.beotkkotthon.rebook.exception.ErrorCode;
 import org.goormthon.beotkkotthon.rebook.repository.StudyHistoryRepository;
-import org.goormthon.beotkkotthon.rebook.usecase.studyhistory.ReadStudyHistoryListUseCase;
 import org.goormthon.beotkkotthon.rebook.usecase.studyhistory.ReadStudyHistoryUseCase;
-import org.goormthon.beotkkotthon.rebook.usecase.studyhistory.UpdateStudyHistoryUseCase;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
 public class ReadStudyHistoryService implements ReadStudyHistoryUseCase {
     private final StudyHistoryRepository studyHistoryRepository;
 
-    private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
     @Override
     public StudyHistoryDetailDto execute(Integer studyHistoryId) {
         StudyHistory studyHistory = studyHistoryRepository.findById(studyHistoryId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
+        String Date = studyHistory.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
         return StudyHistoryDetailDto.builder()
                 .imageUrl(studyHistory.getImageUrl())
                 .content(studyHistory.getRecycleCategory().getDescription())
-                .createdAt(dateFormat.format(studyHistory.getCreatedAt()))
-                .build();
+                .createdAt(Date).build();
     }
 }
